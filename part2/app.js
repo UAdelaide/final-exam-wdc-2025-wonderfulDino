0dccdaa2fc6e090b28dc17597bcfd60f7e8b42ae
wonderfulDino <hi@hi.com>
2025-06-20 10:57:38 +0000

    [automatic commit] 2025-06-20 10:57:38 UTC 144.6.17.218

--- part2/app.js
+++ part2/app.js
@@ -112,7 +112,7 @@ app.get('/dogList', async (req, res) => {
     JOIN
         Users ON Dogs.owner_id = Users.user_id
     WHERE
-        Users.username = '?'
+        Users.username = 'alice123'
     `;
 
     db.query(query, (err, results) => {
