7a74f1ed807a458fa97c04bdba6236180434f5f9
wonderfulDino <hi@hi.com>
2025-06-20 10:19:56 +0000

    [automatic commit] 2025-06-20 10:19:56 UTC 144.6.17.218

--- part2/app.js
+++ part2/app.js
@@ -58,6 +58,7 @@ app.post('/login', (req, res) => {
         if (results.length === 1) {
             req.session.user = results[0].role;
 
+            // Creating a cookie
             const date = new Date();
             date.setTime(date.getTime() + (604800000)); // Cookie will expire in 7 days
             document.cookie = `username=${results[0].username}; expires=` + date.toUTCString() + "; path=/;";
