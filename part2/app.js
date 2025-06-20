0f830a6914e223cd2184d00a8613f52de7c615e8
wonderfulDino <hi@hi.com>
2025-06-20 10:24:46 +0000

    [automatic commit] 2025-06-20 10:24:46 UTC 144.6.17.218

--- part2/app.js
+++ part2/app.js
@@ -62,8 +62,9 @@ app.post('/login', (req, res) => {
             const date = new Date();
             date.setTime(date.getTime() + (604800000)); // Cookie will expire in 7 days
 
-            
-            document.cookie = `username=${results[0].username}; expires=` + date.toUTCString() + "; path=/;";
+            res.cookie('username', result[0].username, {
+                
+            })
 
             // Redirecting to a page after being successful.
             if (results[0].role === 'owner') {
