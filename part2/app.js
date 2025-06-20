b61d6715e4f1972d7e2455ced74ddc2969c5746c
wonderfulDino <hi@hi.com>
2025-06-20 10:50:01 +0000

    [automatic commit] 2025-06-20 10:50:01 UTC 144.6.17.218

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-var mysql = require('mysql2/promise');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
